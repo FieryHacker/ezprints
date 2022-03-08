32bc5e090b4e84eb289741eda567e72cd0b0e95e
JP Nguyen <34077488+FieryHacker@users.noreply.github.com>
2022-03-07 23:15:26 -0500

    Update packages.js

--- assets/js/packages.js
+++ assets/js/packages.js
@@ -1,3 +1,4 @@
+//If the screen is relatively big
 const standard = document.getElementById("standard");
 const silver = document.getElementById("silver");
 const gold = document.getElementById("gold");
@@ -18,4 +19,23 @@ gold.addEventListener('click', function (event) {
   document.getElementById("selected-standard").style.display = "none";
   document.getElementById("selected-silver").style.display = "none";
   document.getElementById("selected-gold").style.display = "flex";
+});
+
+//If the screen is about a Chromebook or lower
+const dropdown = document.getElementById("packages");
+
+dropdown.addEventListener('change', function (event) {
+  if (dropdown.options[dropdown.selectedIndex].value.localeCompare("one") == 0) {
+    document.getElementById("selected-standard").style.display = "flex";
+    document.getElementById("selected-silver").style.display = "none";
+    document.getElementById("selected-gold").style.display = "none";
+  } else if (dropdown.options[dropdown.selectedIndex].value.localeCompare("two") == 0) {
+    document.getElementById("selected-standard").style.display = "none";
+    document.getElementById("selected-silver").style.display = "flex";
+    document.getElementById("selected-gold").style.display = "none";
+  } else if (dropdown.options[dropdown.selectedIndex].value.localeCompare("three") == 0) {
+    document.getElementById("selected-standard").style.display = "none";
+    document.getElementById("selected-silver").style.display = "none";
+    document.getElementById("selected-gold").style.display = "flex";
+  }
 });
